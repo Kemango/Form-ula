09c8a9bffec6f7a89adaa526b5b4ff77c3536a63
Guo Keming <e1398747@u.nus.edu>
2026-01-22 01:54:32 +0800

    Implementing Navigation Bar & Import Material UI for Button and Icon
    Import Material UI
    - Button
    - Icon
    
    Implement
    - NavBar
    
    Things to do next the body
    - Cards
    - Button
    - input/Output Forum

--- form-ula/app/layout.tsx
+++ form-ula/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "GreatFronEnd: Formula",
+  title: "GreatFrontEnd: Formula",
   description: "Take home assignment",
 };
 
