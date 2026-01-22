0cf8a595d151ea5739a0c5fffaef3159e152eff8
Guo Keming <e1398747@u.nus.edu>
2026-01-22 15:38:28 +0800

    Each type of form is move to component file

--- form-ula/components/input/rightSidebar.tsx
+++ form-ula/components/input/rightSidebar.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import Card from '@mui/material/Card';
-import { FormControlLabel, FormGroup, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
-import Checkbox from '@mui/material/Checkbox';
+import { IndivPart } from './indivPart';
 
 export const RightSidebar = () => {
   const [title, settitle] = useState('');
@@ -59,42 +57,9 @@ export const RightSidebar = () => {
           </div>
           {/* Body */}
           <div className="flex items-center justify-center">
-            <Card className="p-10 w-full bg-gray-100">
-              {/* Header */}
-              <div className="mb-4 flex">
-                <TextField
-                  fullWidth
-                  placeholder="New Text Field"
-                  variant="standard"
-                  value={title}
-                  onChange={(e) => settitle(e.target.value)}
-                  InputProps={{
-                    disableUnderline: true,
-                    style: { fontWeight: 'bold'},
-                  }}
-                />
-                <FormGroup>
-                  <FormControlLabel control={<Checkbox defaultChecked />} label="required" />
-                </FormGroup>
-                <IconButton aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </div>
-              <TextField
-                  fullWidth
-                  placeholder="Follow the word above"
-                  value={title}
-                  onChange={(e) => settitle(e.target.value)}
-                  InputProps={{
-                    disableUnderline: true,
-                    style: { fontWeight: 'bold'},
-                  }}
-                />
-            </Card>
-
-
-            {/* <p className="text-gray-500">No form fields added yet. Use the panel on the left to add form elemets</p> */}
+            <p className="p-10">No form fields added yet. Use the panel on the left to add form elemets</p>
           </div>
+          {/* <IndivPart/> */}
         </Card>
       </div>
   );
