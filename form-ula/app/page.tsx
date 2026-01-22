b98b1bb2d49156c911c81eb10b510ee94bd3794d
Guo Keming <e1398747@u.nus.edu>
2026-01-22 12:33:17 +0800

    Created Navbar Componet
    Moving Navbar into a component can make code re-usable and mantainable

--- form-ula/app/page.tsx
+++ form-ula/app/page.tsx
@@ -10,35 +10,16 @@ import SelectIcon from '@mui/icons-material/FormatListBulleted';
 import { useRouter } from 'next/navigation';
 import { Grid } from "@mui/material";
 
+// Component
+import { NavBar } from "@/components/navbar";
+
 
 const Home = () => {
   const router = useRouter(); 
   return (
     <main className="flex min-h-screen flex-col">
       {/* Header NavBar */}
-      <div className="flex boder-b shadow-md border-gray-200">
-        <div className="max-w-6xl w-full mx-auto flex items-center justify-between py-4 px-8">
-          {/* Logo and CompanyName */}
-          <div className="flex gap-3 items-center">
-            <Image
-              src="/NavLogo.jpeg"
-              alt="GreatFrontEnd Logo"
-              width={35}
-              height={35}
-            />
-            <h1 className="font-bold items-center text-3xl">Form Builder</h1>
-          </div>
-          {/* Save and Preview Button */}
-          <div className="flex items-center gap-4">
-            <Button variant="outlined" startIcon={<SendIcon/>} sx={{ color: 'black', borderColor: 'black'}}>
-              <h1>Save</h1>
-            </Button>
-            <Button variant="outlined" sx={{ color: 'black', borderColor: 'black'}} onClick={() => router.push('/PreviewForm')}>
-              Preview Form
-            </Button>
-          </div>
-        </div>
-      </div>
+      <NavBar />
       {/* Body */}
       <div
         className="grid md:grid-cols-[30%_70%]">
